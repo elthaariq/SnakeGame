89fd361317f534ffca60aa3fd47f62449db03264
El Thaariq Is'ad <eriq.isad@gmail.com>
2023-01-09 05:27:42 +0700

    #4
    ....

--- snake-greedy.py
+++ snake-greedy.py
@@ -30,7 +30,7 @@ black = pygame.Color(0, 0, 0)
 white = pygame.Color(255, 255, 255)
 red = pygame.Color(255, 0, 0)
 green = pygame.Color(0, 255, 0)
-darkgreen = pygame.Color(29, 62, 4)
+darkgreen = pygame.Color(6, 40, 0)
 blue = pygame.Color(0, 0, 255)
 
 # Show game over message
